57ac397d78ed6492314f6b92b236d2b627a61ace
Ununavailable <leminhduc264994@gmail.com>
2026-02-03 01:20:37 +0800

    Modify evaluation script

--- src/HSI_CubeEvaluation.py
+++ src/HSI_CubeEvaluation.py
@@ -1,6 +1,11 @@
 from scipy.io import loadmat
-data = loadmat(r"E:/Liam's Projects/HSI-3DCNN-Project/hsi_datasets/v303/Spectrum-Simplified.mat")
-cube = data['DataCube']
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the .mat file
+file_name = "Spectrum-Simplified.mat"
+file_path = loadmat(r"E:/Liam's Projects/HSI-3DCNN-Project/hsi_datasets/v303/" + file_name)
+cube = file_path['DataCube']
 print(f"Shape: {cube.shape}")
 print(f"Dtype: {cube.dtype}")
 print(f"Range: [{cube.min()}, {cube.max()}]")
@@ -9,6 +14,11 @@ print(f"Range: [{cube.min()}, {cube.max()}]")
 plt.plot(cube[45, 100, :])  # Assuming (Y, X, B)
 plt.title("Spectral signature")
 plt.show()
+
+# cube = data['DataCube']
+cube = np.transpose(cube, (1,2,0))  # Fix in memory
+print(f"New shape: {cube.shape}")  # Should be (1632, 1232, 110)
+plt.imshow(cube[:, :, 10], cmap='gray')
 
 # Spatial image at one band
 plt.imshow(cube[:, :, 10], cmap='gray')
